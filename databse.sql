2239e214890642eba56e4109281804e675008d7b
Amir Savand <amir@savandbros.com>
2018-08-15 03:21:18 +0400

    Add databse setup

---
+++ databse.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `song` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `user` varchar(50) NOT NULL,
+ `party` varchar(50) NOT NULL,
+ `url` varchar(50) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
