75c2855e6c07fb73ad25368236ed7264216d4338
Amir Savand <amir@savandbros.com>
2018-12-04 04:37:23 +0400

    Remove flask-angularjs files

--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEBUG = os.environ.get('FLASK_DEBUG', True)
-SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'xxxx')
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DATABASE_URI', 'sqlite:///songs.sqlite3')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-QUERY_LIMIT = os.environ.get('QUERY_LIMIT', 100)
