7b4042f6639d3a22ad7ed8c5dd46320ee86beb92
Amir Savand <amir@savandbros.com>
2019-01-31 22:00:08 +0400

    Add PartyCategory to admin

--- party/admin.py
+++ party/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from party.models import Party, PartyUser
+from party.models import Party, PartyUser, PartyCategory
 
 admin.site.register(Party)
 admin.site.register(PartyUser)
+admin.site.register(PartyCategory)
