18b33cfe7b32932450037ce5d29f118c06805ffb
Amir Savand <amir@savandbros.com>
2020-01-03 20:38:37 +0300

    Setup mail, password reset and password change
    New endpoints:
    
    auth/password/change/
    auth/password/reset/
    auth/password/reset/confirm/

--- playzem/settings/production.py
+++ playzem/settings/production.py
@@ -8,3 +8,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('DJANGO_EMAIL_HOST')
+EMAIL_PORT = os.environ.get('DJANGO_EMAIL_HOST', 587)
+EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD', 'True') == 'True'
